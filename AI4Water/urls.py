ca0486bda8c7d1ad9c0683609a98db1fb01e3456
FazilaRubab <51526391+FazilaRubab@users.noreply.github.com>
2022-06-11 16:30:43 -0700

    Added urls for remaining templates

--- AI4Water/urls.py
+++ AI4Water/urls.py
@@ -1,4 +1,4 @@
-"""AI4Water URL Configuration
+"""ai4waterlatest URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
