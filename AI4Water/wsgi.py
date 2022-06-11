f39a0644d2397c7ab3bc1bc444999f62d5639d38
FazilaRubab <51526391+FazilaRubab@users.noreply.github.com>
2022-06-11 16:35:25 -0700

    Unknown

--- AI4Water/wsgi.py
+++ AI4Water/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for AI4Water project.
+WSGI config for ai4waterlatest project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AI4Water.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ai4waterlatest.settings")
 
 application = get_wsgi_application()
